6b26f7873b30dda8101ac6be468a42330d55d538
BuickSalesman <adam.odonnell.e@gmail.com>
2023-09-27 21:43:08 -0400

    fixed bs

--- src/Content.jsx
+++ src/Content.jsx
@@ -1,12 +1,9 @@
 import axios from "axios";
 import { useState, useEffect } from "react";
 import { JobsIndex } from "./JobsIndex";
-<<<<<<< HEAD
 import { Modal } from "./Modal";
 import { JobsShow } from "./JobsShow";
-=======
 import { JobsNew } from "./JobsNew";
->>>>>>> 53f2ff7c0fe9e151569ceffb8489d5e4798b6f85
 
 export function Content() {
   const [jobs, setJobs] = useState([]);
@@ -33,13 +30,6 @@ export function Content() {
   };
 
   useEffect(handleIndexJobs, []);
-
-  return (
-    <div>
-      <JobsIndex jobs={jobs} onShowJob={handleShowJob} />
-      <Modal show={isJobsShowVisible} onClose={handleClose}>
-        <JobsShow job={currentJob} />
-      </Modal>
   const handleCreateJob = (params, successCallBack) => {
     console.log("handleCreateJob", params);
     axios.post("http://localhost:3000/jobs.json", params).then((response) => {
@@ -48,12 +38,13 @@ export function Content() {
     });
   };
 
-  useEffect(handleIndexPhotos, []);
-
   return (
     <div>
+      <JobsIndex jobs={jobs} onShowJob={handleShowJob} />
       <JobsNew onCreateJob={handleCreateJob} />
-      <JobsIndex jobs={jobs} />
+      <Modal show={isJobsShowVisible} onClose={handleClose}>
+        <JobsShow job={currentJob} />
+      </Modal>
     </div>
   );
 }
