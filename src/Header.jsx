3a4fd97e8071cff27c51966548fe129e1f957d78
masonishee <mishee2010@gmail.com>
2023-09-28 20:25:07 -0500

    Fixed links

--- src/Header.jsx
+++ src/Header.jsx
@@ -2,8 +2,8 @@ export function Header() {
   return (
     <header className="py-2 bg-gray-200">
       <nav>
-        <a href="#">Home</a> | <a href="#">New Job</a> | <a href="./Signup">Signup</a> | <a href="./Login">Login</a> |{" "}
-        <a href="./LogoutLink">Logout</a>
+        <a href="#">Home</a> | <a href="./JobsNew">New Job</a> | <a href="./Signup">Signup</a> |{" "}
+        <a href="./Login">Login</a> | <a href="./LogoutLink">Logout</a>
       </nav>
     </header>
   );
